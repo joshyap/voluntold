6381e92da07317b313ff735adc7471566385528d
Josh Yap <joshua.c.yap@gmail.com>
2022-09-04 10:10:24 -0500

    new roster

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 let peeps = [
-    'cheyne',
-    'court',
-    'cory',
-    'edwin',
-    'hamilton',
-    'jarrett',
-    'jeff',
-    'mario',
-    'nick',
-    'yasir',
-    'yohans'
+    'Damian', 
+    'Daniel', 
+    'Derrick', 
+    'Diana', 
+    'Dominic', 
+    'Dorsa', 
+    'Emre', 
+    'Frederick', 
+    'Greg', 
+    'Gregory', 
+    'Haylee', 
+    'Heather', 
+    'Horacio', 
+    'Jacob', 
+    'Jacob', 
+    'Jake'
 ];
 
 function choose() {
     document.getElementById('winner').innerHTML = peeps[Math.floor(Math.random() * peeps.length)];    
 }
-
 
