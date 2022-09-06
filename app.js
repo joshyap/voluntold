aa30106511ebdf60194902bfbe926c510e77921b
Josh Yap <joshua.c.yap@gmail.com>
2022-09-06 18:28:30 -0500

    update roster

--- app.js
+++ app.js
@@ -4,25 +4,36 @@ let sum = 0;
 let xArray =[];
 let yArray = [];
 
+let config = {responsive: true}
+let layout = { 
+    title: 'VolunTOLD Results'
+  };
 
 let peeps = [
-    ['Damian', 0],
-    ['Daniel', 0],
-    ['Derrick', 0],
-    ['Diana', 0],
-    ['Dominic', 0],
     ['Dorsa', 0],
+    ['Edwin', 0],
     ['Emre', 0],
     ['Frederick', 0],
-    ['Greg', 0],
-    ['Gregory', 0],
+    ['Greg H.', 0],
+    ['Gregory W.', 0],
     ['Haylee', 0],
     ['Heather', 0],
     ['Horacio', 0],
-    ['Jacob G', 0],
-    ['Jacob W', 0],
-    ['Jake', 0]
+    ['Jacob G.', 0],
+    ['Jacob W.', 0],
+    ['Jake L.', 0],
+    ['Jake S.', 0],
+    ['James C.', 0],
+    ['James K.', 0],
+    ['Janvier', 0],
+    ['Jared', 0],
+    ['Jennifer', 0],
+    ['Jeremy', 0],
+    ['John J.', 0],
+    ['John Weaver', 0],
+    ['John Wilkins', 0]
 ];
+
 
 for (let i=0; i<peeps.length; i++) {
     xArray.push(peeps[i][0]); 
@@ -41,7 +52,7 @@ let data = [
 ];
 
 // initial plot rendering
-Plotly.newPlot('chart', data);
+Plotly.newPlot('chart', data, layout, config);
 
 
 function choose() {
@@ -54,6 +65,10 @@ function choose() {
     peeps[randomNumber][1] = peeps[randomNumber][1]+1;
 }
 
+
+
+Plotly.newPlot('myDiv', data, layout, config );
+
 function plot() {
     choose();
 
@@ -65,5 +80,5 @@ function plot() {
         }
     ];
 
-    Plotly.newPlot('chart', data);
+    Plotly.newPlot('chart', data, layout, config);
 }
